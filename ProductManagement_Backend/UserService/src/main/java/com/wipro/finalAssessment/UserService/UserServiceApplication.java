ee21f26bf7f1c7c2637538a231176d384b281b78
Harshahari1207 <harsha733084@gmail.com>
2024-10-21 15:02:39 +0530

    Cors error Fixed.

--- ProductManagement_Backend/UserService/src/main/java/com/wipro/finalAssessment/UserService/UserServiceApplication.java
+++ ProductManagement_Backend/UserService/src/main/java/com/wipro/finalAssessment/UserService/UserServiceApplication.java
@@ -2,12 +2,28 @@ package com.wipro.finalAssessment.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/") 
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
